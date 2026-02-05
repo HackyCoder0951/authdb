aa93c2bbb9595f1b67de052e5514347b9f06015a
hackycoder0951 <jignesh.vision.17@gmail.com>
2026-02-05 22:37:05 +0530

    feat: Update Pydantic settings to use `SettingsConfigDict`, make `MONGODB_URL` and `SECRET_KEY` required, and enable `.env` file loading.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,14 +1,18 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "auth_scale"
-    MONGODB_URL: str = "mongodb+srv://user_authDB:auth245005db@cluster0.pyl0utg.mongodb.net/?appName=Cluster0" # Default to local mongo
+    MONGODB_URL: str
     DB_NAME: str = "auth_scaleDB"
-    SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7" # Change in production
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    class Config:
-        case_sensitive = True
+    model_config = SettingsConfigDict(
+        case_sensitive=True,
+        env_file=[".env", "../.env"],
+        env_ignore_empty=True,
+        extra="ignore"
+    )
 
 settings = Settings()
